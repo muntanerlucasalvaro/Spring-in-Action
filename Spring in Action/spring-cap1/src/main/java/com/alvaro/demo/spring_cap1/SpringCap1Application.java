1442365ce933f01613cc903dd45b66fc972768ce
muntanerlucasalvaro <muntanerlucasalvaro@gmail.com>
2026-08-18 17:21:36 +0200

    Spring in Action cap 1 (0)

---
+++ Spring in Action/spring-cap1/src/main/java/com/alvaro/demo/spring_cap1/SpringCap1Application.java
@@ -0,0 +1,13 @@
+package com.alvaro.demo.spring_cap1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCap1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCap1Application.class, args);
+	}
+
+}
